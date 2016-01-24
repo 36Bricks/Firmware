425cfc72186d18e01fa00d3d1f2153584b7fa12f
Garcia Alexandre <alex@alexetaurore.com>
2016-01-24 16:27:02 +0100

    Corrected IP address serial debug, moved function

--- ConfigFromWifi.ino
+++ ConfigFromWifi.ino
@@ -26,39 +26,6 @@ void setupConfigFromWifi() {
 }
 
 /**
- * Setups a Wifi acces point
- */
-void setupWiFiAP() {
-  Logln("[NFO] Starting WIFI Acces Point !");
-  WiFi.mode(WIFI_AP);
-
-  // Creates AP name using brick type and mac address. ie: "36Brick MultiSensor 36A36A"
-  uint8_t mac[WL_MAC_ADDR_LENGTH];
-  WiFi.softAPmacAddress(mac);
-  String macID = String(mac[WL_MAC_ADDR_LENGTH - 3], HEX) +
-                 String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
-                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
-  macID.toUpperCase();
-  
-  String AP_NameString = "36Brick ";
-  AP_NameString += BRICK_TYPE;
-  AP_NameString += " " + macID;
-  
-  char AP_NameChar[AP_NameString.length() + 1];
-  memset(AP_NameChar, 0, AP_NameString.length() + 1);
-  for (int i=0; i<AP_NameString.length(); i++)
-    AP_NameChar[i] = AP_NameString.charAt(i);
-  
-  Logln("[NFO] AP : " + AP_NameString);
-  WiFi.softAP(AP_NameChar, WiFiAPPSK);      // Starts the Wifi AP
-
-  delay(2000);                              // Waits AP to fully start
-  yield();
-  Serial.print("[NFO] Brick IP : ");
-  Serial.println(WiFi.softAPIP());
-}
-
-/**
  * Serves the setup page
  */
 void WifiSetupPage() {
--- Wifi.ino
+++ Wifi.ino
@@ -4,7 +4,6 @@
 /**
  * Setups the ESP to connect to user wifi
  */
-
 void setupWifi() {
     MDNS.begin(host);
     if (ssidOK && passOK) {
@@ -24,7 +23,7 @@ void setupWifi() {
     
         if (wifiOK) {
             Logln("[NFO] WiFi connected ! ");
-            Logln("[NFO] IP : " + String(WiFi.localIP()));
+            Logln("[NFO] IP : " + WiFi.localIP().toString());
             Logln("[NFO] HTTP Server started ... begin loop");
         } else {
             Logln("[ERR] WiFi not connected, starting AP !");
@@ -33,4 +32,36 @@ void setupWifi() {
     }
 }
 
+/**
+ * Setups a Wifi acces point
+ */
+void setupWiFiAP() {
+  Logln("[NFO] Starting WIFI Acces Point !");
+  WiFi.mode(WIFI_AP);
+
+  // Creates AP name using brick type and mac address. ie: "36Brick MultiSensor 36A36A"
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 3], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+  
+  String AP_NameString = "36Brick ";
+  AP_NameString += BRICK_TYPE;
+  AP_NameString += " " + macID;
+  
+  char AP_NameChar[AP_NameString.length() + 1];
+  memset(AP_NameChar, 0, AP_NameString.length() + 1);
+  for (int i=0; i<AP_NameString.length(); i++)
+    AP_NameChar[i] = AP_NameString.charAt(i);
+  
+  Logln("[NFO] AP : " + AP_NameString);
+  WiFi.softAP(AP_NameChar, WiFiAPPSK);      // Starts the Wifi AP
+
+  delay(2000);                              // Waits AP to fully start
+  yield();
+  Serial.print("[NFO] Brick IP : ");
+  Serial.println(WiFi.softAPIP());
+}
 
